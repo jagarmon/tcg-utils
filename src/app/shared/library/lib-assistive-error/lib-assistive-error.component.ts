5fce1d1db86d17771f1bb1e81d281ebeed79ce9c
jagarmon <xaviercin17@gmail.com>
2025-08-10 00:48:33 +0200

    Changed <p> for component

--- src/app/shared/library/lib-assistive-error/lib-assistive-error.component.ts
+++ src/app/shared/library/lib-assistive-error/lib-assistive-error.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl } from '@angular/forms';
+import { LibFontComponent } from '../lib-font/lib-font.component';
 
 @Component({
   selector: 'lib-assistive-error',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LibFontComponent],
   templateUrl: './lib-assistive-error.component.html',
   styleUrl: './lib-assistive-error.component.scss',
 })
@@ -15,10 +16,10 @@ export class LibAssistiveErrorComponent {
   @Input()
   errorMessages?: Record<string, string>;
   @Input()
-  assistiveText?: string = '';
+  assistiveText = '';
 
   getErrorMessage() {
-    if (!this.control.errors || !this.errorMessages) return null;
+    if (!this.control.errors || !this.errorMessages) return '';
 
     const firstErrorKey = Object.keys(this.control.errors)[0];
 
